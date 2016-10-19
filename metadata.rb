59956eff89c590b2c795670adefdc700068544be
Matvey Aksenov <matvey.aksenov@gmail.com>
2016-10-20 00:13:27 +0300

    Fix foodcritic complaints.

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'matvey.aksenov@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures codestriker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
+source_url       'https://github.com/supki/codestriker'
+issues_url       'https://github.com/supki/codestriker/issues'
