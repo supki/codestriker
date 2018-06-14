c6f15f1ca6f1b250259bfce5a4cc8e42924815e9
Matvey Aksenov <matvey.aksenov@gmail.com>
2018-06-14 20:44:15 +0300

    Remove vulnerable dependencies from Gemfile.lock

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'codestriker'
 maintainer       'Matvey Aksenov'
 maintainer_email 'matvey.aksenov@gmail.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures codestriker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
